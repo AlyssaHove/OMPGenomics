489f7da0b01a2e9ce43c945ce5cfc9f61055e337
Al Hove <hoveax16@juniata.edu>
2019-04-12 13:46:42 -0400

    Starting point

--- OMPGenomics.cpp
+++ OMPGenomics.cpp
@@ -1,21 +1,49 @@
-// OMPGenomics.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*Alyssa Hove and Katheryn Weeden
+Due: 4/19/19
+Design and implement a parallelized openMP C/C++ program that will determine 
+where the standard start and stop codons can be found for a sequence. 
+From which you can determine the length of the actual sequence. Sequences 
+from the DNA analysis will contain leading codes that have to be ignored.
 
-#include "pch.h"
+*/
+
+// input a fasta file into an array. parse out header line and 
+   // eliminate the newlines, etc., until end of file or new header > is found
+   // LKR 4/2017
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+using namespace std;
+int main() {
+	ifstream inFastaFile("yeast.fasta", ios::in);
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+	if (!inFastaFile) {
+		cerr << "File cannot be opened" << endl;
+	}
+
+	char seq[1000000];
+	char header[255];
+	int n = 0;  //elements in the array
+	int lines = 1; //number of lines for the sequence
+
+	char ch;
+
+	//inFastaFile.getline(header, 255);
+
+	//now process input codes until > or end of file
+	while ((ch = inFastaFile.get()) != EOF) {
+		if (ch >= 'A' && ch <= 'Z') seq[n++] = ch;
+		if (ch == '>')break;
+		if (ch == '\n') lines++;
+	}
+
+	cout << "Input complete for sequence: " << header << endl;
+	cout << "The size of the array is " << n << " and there were " << lines
+		<< " lines" << endl;
+	seq[n] = '\0';
+	cout << "The sequence is" << endl;
+	cout << seq << endl;
+	system("pause");
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
